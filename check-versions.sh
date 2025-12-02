f36757a24cccff5f877566cbc158b70b5ae39423
Matias Charriere <matias@giantswarm.io>
2025-12-02 16:39:16 +0100

    update check version
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- check-versions.sh
+++ check-versions.sh
@@ -9,12 +9,13 @@ arr02=("app-admin/cilium-cli-bin" "cilium/cilium-cli")
 arr03=("app-admin/dyff-bin" "homeport/dyff")
 arr04=("app-admin/egctl-bin" "envoyproxy/gateway")
 arr05=("app-admin/k6-bin" "grafana/k6")
-arr06=("app-admin/lastpass-cli" "lastpass/lastpass-cli")
+arr06=("app-misc/gwctl" "kubernetes-sigs/gwctl")
 arr07=("app-admin/teleport-bin" "gravitational/teleport")
 arr08=("app-containers/kubeswitch" "danielfoehrKn/kubeswitch")
 arr09=("app-misc/omm" "dhth/omm")
 arr10=("app-misc/dasel" "TomWright/dasel")
-arr=(arr01 arr02 arr03 arr04 arr05 arr06 arr07 arr08 arr09 arr10)
+arr11=("app-misc/fluxcd-bin" "fluxcd/flux2")
+arr=(arr01 arr02 arr03 arr04 arr05 arr06 arr07 arr08 arr09 arr10 arr11)
 
 declare -n elmv1 elmv2
 
@@ -29,5 +30,6 @@ for elmv2 in "${arr[@]}"; do
 done
 
 exit 0
+
 
 
